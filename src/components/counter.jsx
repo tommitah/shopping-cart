addc0ee69d19e9e592fe03d6e4d9783c88d4e02b
tommitah <reijotahvanainen15@gmail.com>
2021-01-06 17:12:52 +0200

    encapsulated behaviour out of the render method.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,37 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  styles = {
+    fontSize: 20,
+    fontWeight: "bold",
   };
 
   render() {
     return (
-      <React.Fragment>
-        <span>{this.state.count}</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className={this.getBadgeClass()}>{this.formatCounter()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
     );
+  }
+
+  getBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCounter() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
