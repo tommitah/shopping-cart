33765246ae4f181b1dbc5a3c9aaf0d3a5e74c8e0
tommitah <reijotahvanainen15@gmail.com>
2021-01-07 13:29:42 +0200

    count => value, mapattu counterit arrayhyn.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
   styles = {
@@ -21,11 +21,11 @@ class Counter extends Component {
   // eikä myöskään "bindata" funktio-oliota (this.handleIncrement = this.handleIncrement.bind(this);)
   // HUOM!! funktiot javascriptissä on myös olioita, tämän takia voit kutsua bind() metodia eventhandlerille.
   handleIncrement = (product) => {
-    // Reactissa ei kosketa stateen suoraan, esim. this.state.count++;
+    // Reactissa ei kosketa stateen suoraan, esim. this.state.value++; (Voi olla vanhaa tietoa!)
     // Sen sijaan passataan muutos metodille joka updatee DOMin, jotta muutos renderöidään oikein
     // setState ottaa sisäänsä olion.
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    console.log(product); // PRODUCT ON UNDEFINED!
+    this.setState({ value: this.state.value + 1 });
   };
 
   // Rajapintaistaa alkuperäisen handlerin. Ohjelma osaa antaa handlerille parametrin, joka voi olla esim. tuotteen tunniste.
@@ -40,7 +40,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClass()}>{this.formatCounter()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -51,13 +51,13 @@ class Counter extends Component {
 
   getBadgeClass() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCounter() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
