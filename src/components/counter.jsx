b06599b3bb81c6a39709639191126a3e001cede4
tommitah <reijotahvanainen15@gmail.com>
2021-01-07 14:07:09 +0200

    Ongelmia standardin kanssa, parametrin inline.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,6 +4,7 @@ class Counter extends Component {
   // ---Props vs State---
   // Props sisältää dataa jota annetaan komponentille, State sisältää dataa joka on lokaalia tai yksityistä(private)
   // ko. komponentille.
+  // "The component that _owns_ a piece of the state, should be the one _modifying_ it."
   state = {
     value: this.props.value,
   };
@@ -24,11 +25,10 @@ class Counter extends Component {
   // Kun käyttää nuolifunktiota, ei ole tarpeellista kutsua luokan konstruktoria ja base luokan kon. (super();)
   // eikä myöskään "bindata" funktio-oliota (this.handleIncrement = this.handleIncrement.bind(this);)
   // HUOM!! funktiot javascriptissä on myös olioita, tämän takia voit kutsua bind() metodia eventhandlerille.
-  handleIncrement = (product) => {
+  handleIncrement = () => {
     // Reactissa ei kosketa stateen suoraan, esim. this.state.value++; (Voi olla vanhaa tietoa!)
     // Sen sijaan passataan muutos metodille joka updatee DOMin, jotta muutos renderöidään oikein
     // setState ottaa sisäänsä olion.
-    console.log(product); // PRODUCT ON UNDEFINED!
     this.setState({ value: this.state.value + 1 });
   };
 
@@ -48,6 +48,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,19 +2,32 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
+  // Kun poistetaan Counter listasta, behaviourin _on oltava_ tässä komponentissa (katso kommentti counter.jsx), vaikka nappi itsessään
+  // on osa Counter-luokkaa.
+  // Koska this.state on private, ei Counters-luokan child Counter pääse käsiksi tähän dataan, joten käytetään eventtiä.
   state = {
     counters: [
-      { id: 0, value: 0 },
       { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
+      { id: 4, value: 0 },
     ],
   };
+
+  handleDelete = (counterId) => {
+    console.log("Event handler called!", counterId);
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} selected={true} />
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+            selected={true}
+          />
         ))}
       </div>
     );
