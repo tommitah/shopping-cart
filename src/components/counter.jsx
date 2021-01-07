3a10ad6182638a121854439c09881e6e51c0564e
tommitah <reijotahvanainen15@gmail.com>
2021-01-07 12:41:28 +0200

    Error: tutorial deprecated, product on undef.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,12 +15,32 @@ class Counter extends Component {
   //  this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
+  // Event handler!
+  // See constructor!
+  // Kun käyttää nuolifunktiota, ei ole tarpeellista kutsua luokan konstruktoria ja base luokan kon. (super();)
+  // eikä myöskään "bindata" funktio-oliota (this.handleIncrement = this.handleIncrement.bind(this);)
+  // HUOM!! funktiot javascriptissä on myös olioita, tämän takia voit kutsua bind() metodia eventhandlerille.
+  handleIncrement = (product) => {
+    // Reactissa ei kosketa stateen suoraan, esim. this.state.count++;
+    // Sen sijaan passataan muutos metodille joka updatee DOMin, jotta muutos renderöidään oikein
+    // setState ottaa sisäänsä olion.
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  // Rajapintaistaa alkuperäisen handlerin. Ohjelma osaa antaa handlerille parametrin, joka voi olla esim. tuotteen tunniste.
+  // ällö tapa kirjoittaa tämä toiminnallisuus, älä tee ylimääräisiä wrappereita, joita kutsutaan renderissä oikean funktion sijasta.
+  // Sen sijaan käytä nuolifunktiota suoraan render() funktion sisällä ts. inline function.
+  // doHandleIncrement = () => {
+  //  this.handleIncrement({ id: 1 });
+  // };
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClass()}>{this.formatCounter()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -28,18 +48,6 @@ class Counter extends Component {
       </div>
     );
   }
-
-  // Event handler!
-  // See constructor!
-  // Kun käyttää nuolifunktiota, ei ole tarpeellista kutsua luokan konstruktoria ja base luokan kon. (super();)
-  // eikä myöskään "bindata" funktio-oliota (this.handleIncrement = this.handleIncrement.bind(this);)
-  // HUOM!! funktiot javascriptissä on myös olioita, tämän takia voit kutsua bind() metodia eventhandlerille.
-  handleIncrement = () => {
-    // Reactissa ei kosketa stateen suoraan, esim. this.state.count++;
-    // Sen sijaan passataan muutos metodille joka updatee DOMin, jotta muutos renderöidään oikein
-    // setState ottaa sisäänsä olion.
-    this.setState({ count: this.state.count++ });
-  };
 
   getBadgeClass() {
     let classes = "badge m-2 badge-";
